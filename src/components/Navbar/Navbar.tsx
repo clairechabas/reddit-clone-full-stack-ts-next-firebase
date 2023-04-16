3f94650ce5d55114f5d85f93557cf0347b926242
Claire Chabas <claire.chabas@gmail.com>
2023-04-16 22:36:24 +0200

    feat: navbar with logo, search input and auth buttons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,28 @@
+import { Flex, Image } from '@chakra-ui/react'
 import React from 'react'
+import SearchInput from './SearchInput'
+import RightContent from './RightContent'
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image
+          src="/images/redditFace.svg"
+          height="30px"
+          alt="Reddit mascot logo"
+        />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          alt="Reddit texto logo"
+          display={{ base: 'none', md: 'unset' }}
+        />
+      </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      <RightContent />
+    </Flex>
+  )
 }
 export default Navbar
