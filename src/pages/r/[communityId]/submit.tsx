44256131d07d844f758ea5e8af7bae80064699c3
Claire Chabas <claire.chabas@gmail.com>
2023-04-19 10:35:41 +0200

    feat: Init new post form and create form tabs ui

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,6 +1,22 @@
+import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
+import PageContent from '../../../components/Layout/PageContent'
+import NewPostForm from '@/src/components/Posts/NewPostForm'
 
 const SubmitPostPage: React.FC = () => {
-  return <div>Submit page</div>
+  return (
+    <PageContent>
+      {/* New Post Form */}
+      <>
+        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text>Create a post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+
+      {/* About */}
+      <></>
+    </PageContent>
+  )
 }
 export default SubmitPostPage
