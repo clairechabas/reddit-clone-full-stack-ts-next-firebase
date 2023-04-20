7f7b57f6b98b09eaaccdc33b3fc419de3ee41c08
Claire Chabas <claire.chabas@gmail.com>
2023-04-20 14:09:50 +0200

    fix: handling deploy errors

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,5 +1,6 @@
 import { SearchIcon } from '@chakra-ui/icons'
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { User } from 'firebase/auth'
 import React from 'react'
 
 type SearchInputProps = {
