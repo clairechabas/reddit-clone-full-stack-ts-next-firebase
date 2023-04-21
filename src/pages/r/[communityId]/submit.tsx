815c42d904c18c13c81b68b1f081fadb94da6a7d
Claire Chabas <claire.chabas@gmail.com>
2023-04-21 12:19:48 +0200

    fix: yet another path change test + removing useless comment

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -13,7 +13,6 @@ const SubmitPostPage: React.FC = () => {
 
   return (
     <PageContent>
-      {/* New Post Form */}
       <>
         <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
           <Text>Create a post</Text>
