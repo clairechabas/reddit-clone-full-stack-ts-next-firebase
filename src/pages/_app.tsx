2dc7d94ad09f6c45c35a096357e9a33e64c878e2
Claire Chabas <claire.chabas@gmail.com>
2023-04-21 11:09:53 +0200

    fix: updating ts version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,8 @@ import { theme } from '../ui/theme'
 
 import Layout from '@/src/components/Layout'
 import { RecoilRoot } from 'recoil'
-import { NextPage } from 'next'
-import { ReactElement, ReactNode } from 'react'
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-function App({ Component, pageProps }: AppPropsWithLayout) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
