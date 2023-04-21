8299679a0c3d2b6a943611b829ac9e3b127d7410
Claire Chabas <claire.chabas@gmail.com>
2023-04-21 10:43:34 +0200

    fix: handling deploy errors

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 
-interface AppLayoutProps {
+interface LayoutProps {
   children: React.ReactNode
 }
 
-const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Navbar />
@@ -13,4 +13,4 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
     </>
   )
 }
-export default AppLayout
+export default Layout
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from '../ui/theme'
 
-import AppLayout from '@/src/components/Layout/AppLayout'
+import Layout from '@/src/components/Layout'
 import { RecoilRoot } from 'recoil'
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
-        <AppLayout>
+        <Layout>
           <Component {...pageProps} />
-        </AppLayout>
+        </Layout>
       </ChakraProvider>
     </RecoilRoot>
   )
 }
+
+export default App
