2e63d74ba314566d6c76151ced2ace573e331685
Claire Chabas <claire.chabas@gmail.com>
2023-04-21 14:36:19 +0200

    fix: rebuilding layout and

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from '../ui/theme'
 
-import Navbar from '@/components/Navbar/Navbar'
+import Layout from '@/components/Layout/Layout'
 import { RecoilRoot } from 'recoil'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
-        <>
-          <Navbar />
+        <Layout>
           <Component {...pageProps} />
-        </>
+        </Layout>
       </ChakraProvider>
     </RecoilRoot>
   )
