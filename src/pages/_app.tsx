ce13876809fff34ba002b44d4b9986c7558dc4e9
Claire Chabas <claire.chabas@gmail.com>
2023-04-21 11:36:07 +0200

    fix: adding out dir and updating path to main layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from '../ui/theme'
 
-import Layout from '@/src/components/Layout'
+import Layout from '../components/Layout'
 import { RecoilRoot } from 'recoil'
 
 function App({ Component, pageProps }: AppProps) {
